2a08d43ebd759d472b9f9f8abc00d326924ea7c9
jle0010 <48145018+jle0010@users.noreply.github.com>
2019-04-15 10:29:24 -0500

    Add files via upload

--- littleStuffServer.php
+++ littleStuffServer.php
@@ -1,4 +1,7 @@
 <?php
+    if ( isset( $_REQUEST['mydata'] ) ) {
+        $mydata = ( $_REQUEST['mydata'] );
+    }
   $mydata = $_REQUEST["mydata"];
   $rando = rand(144000, 1008000);
   usleep($rando);
