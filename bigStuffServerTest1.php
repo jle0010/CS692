9d910a31525f764238c3bbfb860f2ab982510389
jle0010 <48145018+jle0010@users.noreply.github.com>
2019-04-15 16:16:23 -0500

    Add files via upload

---
+++ bigStuffServerTest1.php
@@ -0,0 +1,8 @@
+<?php
+  $mydata = filter_var($HTTP_GET_VARS["mydata"], FILTER_SANITIZE_STRING);
+  $rando = rand(500, 6000);
+  usleep($rando);
+  //echo "$rando";
+  echo htmlspecialchars("$rando", ENT_COMPAT, 'UTF-8');
+  //echo esc_html__("$rando");
+?>
