ee33299c40c31f27791c929632e28a284bf0c772
jle0010 <48145018+jle0010@users.noreply.github.com>
2019-04-16 14:09:41 -0500

    Update bigStuffServerTest1.php

--- bigStuffServerTest1.php
+++ bigStuffServerTest1.php
@@ -4,5 +4,5 @@
   usleep($rando);
   //echo "$rando";
   echo htmlspecialchars("$rando", ENT_COMPAT, 'UTF-8', FILTER_SANITIZE_STRING);
-  //echo esc_html__("$rando");
+  echo esc_html__("$rando", FILTER_SANITIZE_STRING);
 ?>
