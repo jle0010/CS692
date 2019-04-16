f892f24ce5e5386e732f3ec1194a05708a8c21cc
jle0010 <48145018+jle0010@users.noreply.github.com>
2019-04-16 13:51:20 -0500

    Update bigStuffServerTest1.php

--- bigStuffServerTest1.php
+++ bigStuffServerTest1.php
@@ -3,6 +3,6 @@
   $rando = rand(500, 6000);
   usleep($rando);
   //echo "$rando";
-  //echo htmlspecialchars("$rando", ENT_COMPAT, 'UTF-8');
-  echo esc_html__("$rando");
+  echo htmlspecialchars("$rando", ENT_COMPAT, 'UTF-8');
+  //echo esc_html__("$rando");
 ?>
