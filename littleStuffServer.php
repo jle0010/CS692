15f2325d4e830d45f8e6ff5e1922022c42f59faf
jle0010 <48145018+jle0010@users.noreply.github.com>
2019-04-18 15:12:18 -0500

    Add files via upload

--- littleStuffServer.php
+++ littleStuffServer.php
@@ -1,5 +1,5 @@
 <?php
-    $mydata = filter_var($HTTP_GET_VARS['mydata'], FILTER_SANITIZE_STRING);
+    $mydata = filter_input(INPUT_GET, "mydata", FILTER_SANITIZE_STRING);
     $rando = rand(144000, 1008000);
     usleep($rando);
     echo intval($rando);
